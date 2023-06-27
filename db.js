d2f3b3338a9b46a2a70facef6ecaafa1cf164bf8
Arpan Singh <arpansinghrajput123@gmail.com>
2023-06-28 00:18:12 +0530

    Forget password functionality added

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // Require to don;t get undefined value of env file
-const mongooseUrl = 'mongodb+srv://arpansinghrajput123:Arpan123@userdetails.jbkukmy.mongodb.net/';
+const mongooseUrl = process.env.Mongoose_Url;
 
 // Mongoose Connection function
 const connectToMongoodb =()=>{
